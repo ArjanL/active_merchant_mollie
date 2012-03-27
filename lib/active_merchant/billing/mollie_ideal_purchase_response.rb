b83f2ed45626790b0deaa341f608cab86687a594
Arjan Landman <landman.arjan@gmail.com>
2012-03-27 22:58:45 +0200

    new commits

---
+++ lib/active_merchant/billing/mollie_ideal_purchase_response.rb
@@ -0,0 +1,23 @@
+module ActiveMerchant #:nodoc:
+  module Billing #:nodoc:
+    class MollieIdealPurchaseResponse < Response
+    
+      def token
+        @params['token]
+      end
+      
+      def amount
+        @params['amount']
+      end        
+      
+      def url
+        @params['url']
+      end
+      
+      def currency
+        @params['currency']
+      end
+      
+    end
+  end
+end
